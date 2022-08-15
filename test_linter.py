aae7e0b3e34c8e349ff4232a897b83a00cdd5fc0
mguinhos <razyrabbit@gmail.com>
2022-08-14 21:42:38 -0300

    test_linter.py: Update modules names

--- test_linter.py
+++ test_linter.py
@@ -1,12 +1,12 @@
 from greek.control import Control
 from greek import lexer
 from greek import parser
-from greek import linter
+from greek import checker
 
 from pprint import pprint
 
 tokens = list(lexer.lex(Control(open('examples/hello_world.greek').read())))
 asts = list(parser.parse(Control(tokens)))
-scope = linter.lint(asts)
+scope = checker.check(asts)
 
 print(repr(scope))
