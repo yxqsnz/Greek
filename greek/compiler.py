c9664ebede2ea1c6270a320574f757cbd64d23cc
mguinhos <razyrabbit@gmail.com>
2022-08-14 22:15:40 -0300

    greek/compiler.py: Improve compile_call for struct methods

--- greek/compiler.py
+++ greek/compiler.py
@@ -113,7 +113,12 @@ def compile_call(scope: Scope, call: Call, direct=False):
         
     if type(function) is not ExternFunction and function.owner is not None:
         call_module, _ = _get_dot_bases(call.name.value)
-        compiled_expressions = (call_module.replace(".", "__"), *(compile_expression(scope, argument) for argument in call.arguments))
+        
+        if Name("self") is function.parameters:
+            compiled_expressions = (call_module.replace(".", "__"), *(compile_expression(scope, argument) for argument in call.arguments))
+        else:
+            compiled_expressions = tuple(compile_expression(scope, argument) for argument in call.arguments)
+
         return f'{function.owner.kind.as_name.value}__{function.name.value}({", ".join(compiled_expressions)})'
 
     if direct:
